96e7903da0999ab5a8cf8c96e7b699905378d04c
Andrea Angquist <andrea@elementum.com>
2014-02-21 14:32:17 -0800

    JS TEST

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -14,6 +14,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
+$('.dropdown-toggle').dropdown()
 
 +function ($) {
   'use strict';
